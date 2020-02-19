d22fc8eac0ebb180752c3a313299c7be438f85cc
Elya <elfucio@gmail.com>
2020-02-20 02:35:57 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+  if (Array.isArray(array) && array.length > 0) {
+      let sortedArr = array.sort((a, b) => (a - b));
+      return sortedArr[0];
+  } else return 0;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.max =  function max (array) {
+  if (Array.isArray(array) && array.length > 0) {
+  let sortedArr = array.sort((a, b) => (a - b)).reverse();
+  return sortedArr[0];
+} else return 0;
+};
 
 exports.avg = function avg (array) {
-  return 0;
-}
+  if (Array.isArray(array) && array.length > 0) {
+  let avgNum = 0;
+  avgNum = array.reduce((a, b) => (a + b)) / array.length;
+  return avgNum;
+} else return 0;
+};
+
